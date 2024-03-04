b025e036ecea52fe7b5028e8615ee24b6867db42
Lakshminarayana7844 <121929348+Lakshminarayana7844@users.noreply.github.com>
2024-03-04 10:56:18 +0530

    Update Day1Q1A.R

--- DHAV day1/Day1Q1A.R
+++ DHAV day1/Day1Q1A.R
@@ -1,5 +1,13 @@
-v <- c(15000,18000,22000,20000,23000)
+v <- c(15000, 18000, 22000, 20000, 23000)
+months <- c("Jan", "Feb", "Mar", "Apr", "May")
+
+x_values <- 1:length(v)
+
 png(file = "Q1.jpg")
-plot(v,type = "o")
+
+plot(x_values, v, type = "l", xlab = "Months", ylab = "Sales", main = "Line Plot", xaxt = "n")
+axis(side = 1, at = x_values, labels = months)
+
 title(main = "Line Plot")
+
 dev.off()
